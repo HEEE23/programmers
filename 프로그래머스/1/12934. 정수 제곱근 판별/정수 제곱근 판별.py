1a046076224fd6125567ce7f16a7b2902434a3d9
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-07-05 23:38:53 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+
+    lst = []
+    for i in range(1,n+1):
+        if ( (i == n/i) and (n%i == 0)):
+            lst.append(i)
+            break
+            
+    if len(lst) == 0:
+        answer = -1
+    else:
+        answer = (lst[0]+1)*(lst[0]+1)
+        
+    return answer
+
+    
