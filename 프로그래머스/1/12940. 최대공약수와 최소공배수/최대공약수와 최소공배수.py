05f6dbf863f480c6cc86bfa537b8a88401861d61
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-07-08 00:45:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,16 +1,12 @@
 def solution(n, m):
-    answer = []
-    
     lst = []
-    
     for i in range(1,min(n,m)+1):
         if (n%i == 0) and (m%i == 0):
             lst.append(i)
     
-    gcf = max(lst) # 최대 공약수
+    gcd = max(lst) # 최대 공약수
 
-    lcm = gcf * (n/gcf) * (m/gcf) # 최소 공배수
+    lcm = gcd * (n/gcd) * (m/gcd) # 최소 공배수
 
-    answer.append(gcf)
-    answer.append(lcm)
+    answer = [gcd, lcm]
     return answer
