35d66898847622668fbf9c0f8fa968893c0b9161
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-07-19 22:47:25 +0900

    [level 1] Title: 옹알이 (2), Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,13 @@
+def solution(babbling):
+    answer = 0
+    pron = ['aya','ye','woo','ma']
+    
+    for b in babbling:
+        for p in pron:
+            if p*2 not in b:
+                b = b.replace(p,' ')
+                
+        if b.isspace():
+             answer += 1   
+                
+    return answer
