ebb43735bbf4d94f0cd0f275048b89d11a719b2b
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-07-21 02:08:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.77 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,22 @@
+def solution(participant, completion):
+    answer = ''
+    
+#     for c in completion:
+#         if c in participant:
+#             participant.remove(c)
+            
+#     answer = ''.join(participant)
+    
+    dic = {}
+    for p in participant:
+        dic[p] = dic.get(p, 0) + 1 
+    
+    
+    for c in completion:
+        dic[c] -= 1
+        
+    for k, v in dic.items():
+        if v > 0:
+            answer = k
+
+    return answer
