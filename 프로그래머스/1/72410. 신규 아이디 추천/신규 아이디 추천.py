41675f0fd18113f88b8993876fa4d0219f8d1746
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-08-01 01:47:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,16 +1,20 @@
 def solution(new_id):
     answer = ''
     
+    # 1
     answer = new_id.lower()
     
+    # 2
     cha = ['`','~','!','@','#','$','%','^','&','*','(',')','=',';',':',',','"','<','>','/','?','+','[',']','{','}','\\']
            
     for c in cha:
         answer = answer.replace(c, '')
-        
+    
+    # 3
     while '..' in answer:
         answer = answer.replace('..','.')
     
+    # 4
     if len(answer) != 0:
         if answer[0] == '.':
             answer = answer[1:]
@@ -19,16 +23,17 @@ def solution(new_id):
         if answer[-1] == '.':
             answer = answer[:-1]
     
+    # 5
     if len(answer) == 0:
         answer += 'a'
-        
+    
+    # 6
     if len(answer) >= 16:
         answer = answer[:15]
-            
-    if len(answer) != 0:
         if answer[-1] == '.':
             answer = answer[:-1]
-            
+    
+    # 7
     if len(answer) <= 2:
         while len(answer) != 3:
             answer += answer[-1]
