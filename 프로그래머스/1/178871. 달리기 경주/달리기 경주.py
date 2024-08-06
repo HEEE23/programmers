d414bb1ece3a81bdd9c50e72564663b6949d4664
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-08-07 00:50:33 +0900

    [level 1] Title: 달리기 경주, Time: 921.63 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,15 @@
+def solution(players, callings):
+    answer = []
+
+    player_dict = {player : idx for idx, player in enumerate(players)}
+    
+    for call in callings:
+        idx = player_dict[call]
+        
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+        
+        player_dict[players[idx]] = idx
+        player_dict[players[idx-1]] = idx - 1
+        
+    answer = players
+    return answer
