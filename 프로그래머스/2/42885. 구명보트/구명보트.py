7a2e51563ea3c6d0188f34fd51cc3f6bc7aa5a07
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-08-21 22:58:20 +0900

    [level 2] Title: 구명보트, Time: 13.32 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+
+    people = deque(sorted(people, reverse = True))
+    while len(people) > 1:
+        if people[0] + people[-1] > limit:
+            people.popleft()
+            answer += 1
+        else:
+            people.pop()
+            people.popleft()
+            answer += 1
+            
+    if people:
+        answer += 1
+    return answer
