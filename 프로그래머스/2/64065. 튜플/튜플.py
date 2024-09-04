5dad247e2aff155381d80e2230bad80a0cf85dd8
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-09-05 00:06:51 +0900

    [level 2] Title: 튜플, Time: 178.05 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,29 +1,37 @@
 def solution(s):
     answer = []
     
-    s = list(s)
-    s = s[1:-1]
+#     # 방법 1
+#     s = list(s)
+#     s = s[1:-1]
 
-    num = ''
-    lst1 = [] # 리스트 내 리스트
-    lst2 = [] # 리스트
-    for i in s:
-        if i != '}':
-            if i != '{':
-                if i != ',':
-                    num += i
-                else:
-                    if len(num) != 0:
-                        lst1.append(int(num))
-                        num = ''
-        else:
-            lst1.append(int(num))
-            num = ''
-            lst2.append(lst1)
-            lst1 = []
+#     num = ''
+#     lst1 = [] # 리스트 내 리스트
+#     lst2 = [] # 리스트
+#     for i in s:
+#         if i != '}':
+#             if i != '{':
+#                 if i != ',':
+#                     num += i
+#                 else:
+#                     if len(num) != 0:
+#                         lst1.append(int(num))
+#                         num = ''
+#         else:
+#             lst1.append(int(num))
+#             num = ''
+#             lst2.append(lst1)
+#             lst1 = []
+    
+    # 방법 2
+    s1 = s.lstrip('{').rstrip('}').split('},{')
+    
+    new_s = []
+    for i in s1:
+        new_s.append(list(map(int, i.split(','))))
 
     # 정렬된 리스트 s
-    sorted_s = sorted(lst2, key = len)
+    sorted_s = sorted(new_s, key = len)
           
     for ss in sorted_s:
         for i in ss:
