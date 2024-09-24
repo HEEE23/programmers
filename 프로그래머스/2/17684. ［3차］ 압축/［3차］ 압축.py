7b870163ef3f32ad3603c7f82b876058d7f9fde0
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-09-25 00:11:11 +0900

    [level 2] Title: [3차] 압축, Time: 0.48 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(msg):
+    answer = []
+    
+    # 사전
+    lzw = {}
+    for i in range(26):
+        lzw[chr(65+i)] = i + 1
+    
+    w = c = 0
+    while True:
+        c += 1
+        if c == len(msg):
+            answer.append(lzw[msg[w:c]])
+            break
+        
+        # 다음 글자를 포함한 글자가 사전에 없는 경우
+        if msg[w:c+1] not in lzw:
+            # 사전에 등록
+            lzw[msg[w:c+1]] = len(lzw) + 1
+            # 색인 번호 출력
+            answer.append(lzw[msg[w:c]])
+            w = c
+        # 사전에 있는 경우, c의 값만 증가
+    return answer
