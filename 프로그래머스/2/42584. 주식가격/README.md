e35c96d6f58357173b53c4d5d2016b0edb227f02
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-10-02 23:11:28 +0900

    [level 2] Title: 주식가격, Time: 187.78 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 19.9 MB, 시간: 196.52 ms
+메모리: 19.3 MB, 시간: 187.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 02일 23:07:29
+2024년 10월 02일 23:11:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,12 @@
 def solution(prices):
-    answer = []
+    answer = [0] * len(prices)
     
-    queue = []
     for i in range(len(prices)):
-        queue.append(prices[i])
-        answer.append(0)
-        
         for j in range(i+1, len(prices)):
-            answer[i] += 1
-            
-            if prices[j] < prices[i]:
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
                 break
-
+                
     return answer
