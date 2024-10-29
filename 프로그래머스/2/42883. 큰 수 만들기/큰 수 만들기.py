edebd79bb8b42ff8cf65938bc521bac6e958f1a2
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-10-29 13:45:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.31 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,40 @@
+def solution(number, k):
+    answer = ''
+    
+#     number = list(number)
+    
+#     cnt = 0
+#     while cnt != k:
+#         for i in range(len(number)):
+#             if number[i] > number[i+1]:
+#                 num_min = number[i+1]
+#                 for j in range(i+2,len(number)):
+#                     if number[j] < num_min:
+#                         num_min = number[j]
+#                     else:
+#                         break
+#                 number.remove(num_min)
+#                 cnt+=1
+#                 break
+#             elif number[i] < number[i+1]:
+#                 number.remove(number[i])
+#                 cnt += 1
+#                 break
+
+#     answer = ''.join(number)
+
+    stack = []
+    for n in number:
+        if stack and k > 0:
+            while stack[-1] < n:
+                stack.pop()
+                k -=1
+                if k <= 0 or not stack:
+                    break
+        stack.append(n)
+    
+    answer = ''.join(stack)
+    if k > 0:
+        return answer[:-k]
+    else:
+        return answer
