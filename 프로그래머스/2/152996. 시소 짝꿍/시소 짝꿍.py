149fcb3449aba85ec5a7a2a317932556eb936f68
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-11-04 14:10:20 +0900

    [level 2] Title: 시소 짝꿍, Time: 143.69 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+def solution(weights):
+    answer = 0
+    
+    dic = defaultdict(int)
+    
+    for w in weights:
+        answer += dic[w] + dic[(w*2)/3] + dic[(w*2)/4] + dic[(w*3)/2] + dic[(w*3)/4] + dic[(w*4)/2] + dic[(w*4)/3]
+        dic[w] += 1
+
+    return answer
