e46ef4b781a8ffc770cc75e070a394d507e85901
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-11-08 14:27:16 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,20 +1,17 @@
 def solution(n):                                                        
     answer = ''
     
-    country = [4, 1, 2]
-    lst = []
+    country = ['4', '1', '2']
     
-    q = 1
-    while q != 0:
-        q = n // 3
+    lst = [] 
+    while n != 0:
         r = n % 3
-        if r == 0:
-            q -= 1
-        lst.append(country[r])
-        n -= 1
         n = n // 3
+        if r == 0:
+            n -= 1
+        lst.append(country[r])
 
     lst = lst[::-1]
 
-    answer = ''.join(map(str,lst))
+    answer = ''.join(lst)
     return answer
