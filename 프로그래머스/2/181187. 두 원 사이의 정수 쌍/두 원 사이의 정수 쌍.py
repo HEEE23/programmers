b5268125068c07322b4f5cfbbb040c10735ba33f
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-12-16 22:45:40 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1664.68 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,16 @@
+from math import sqrt, ceil, floor
+
+def solution(r1, r2):
+    answer = 0
+    
+    # 한 사분면 * 4
+    for x in range(1, r2+1) :
+        # x가 정수일 때 y가 정수인 점을 탐색
+        y2 = floor(sqrt(r2**2-x**2))
+        y1 = ceil(sqrt(max(0,(r1**2-x**2))))
+        
+        answer += y2-y1 + 1
+                       
+    answer = answer * 4
+    
+    return answer
