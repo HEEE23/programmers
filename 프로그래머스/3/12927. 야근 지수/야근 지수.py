302a74f8159e8b6aa5449a97dbdb48da85242b89
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-02-01 15:37:27 +0900

    [level 3] Title: 야근 지수, Time: 452.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,33 @@
+import heapq
+def solution(n, works):
+    answer = 0
+    
+    if sum(works) < n:
+        return 0
+    
+    heap = []
+    for w in works:
+        heapq.heappush(heap, -w)
+    
+    while n != 0:
+        work = -heapq.heappop(heap)
+        if work != 0:
+            work -= 1
+            heapq.heappush(heap, -work)
+        n -= 1
+
+    for h in heap:
+        answer += h**2
+        
+    
+#     # 효율성 테스트 실패
+#     while n != 0:
+#         works.sort(reverse = True)
+#         if works[0] > 0:
+#             works[0] -= 1
+#         n -= 1
+    
+#     for w in works:
+#         answer += w**2
+
+    return answer
