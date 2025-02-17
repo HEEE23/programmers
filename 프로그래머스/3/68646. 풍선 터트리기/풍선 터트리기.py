a01b1e08e09e41b9fdbb562f5a028763716aae1b
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-02-17 15:52:46 +0900

    [level 3] Title: 풍선 터트리기, Time: 1162.09 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -0,0 +1,28 @@
+def solution(a):
+    # 양 끝 값은 늘 최후까지 남을 수 있음
+    answer = 2
+    
+    dp = [[0 for _ in range(len(a))] for _ in range(2)]
+    
+    dp[0][0] = a[0]
+    dp[1][-1] = a[-1]
+    
+    # 왼쪽 -> 오른쪽 최솟값
+    for i in range(1,len(a)):
+        dp[0][i] = min(dp[0][i-1], a[i])
+    
+    # 오른쪽 -> 왼쪽 최솟값
+    for i in range(len(a)-2,-1,-1):
+        dp[1][i] = min(dp[1][i+1], a[i])
+        
+    for i in range(1,len(a)-1):
+        left_min = dp[0][i-1]
+        right_min = dp[1][i+1]
+        
+        # 왼쪽 최솟값, 오른쪽 최솟값보다 클 경우
+        if left_min > a[i] and right_min > a[i]:
+            answer += 1
+        # 왼쪽 최솟값보다 작고 오른쪽 최솟값보다 클 경우, 왼쪽 최솟값보다 크고 오른쪽 최솟값보다 작을 경우
+        elif (left_min > a[i] and right_min < a[i]) or (left_min < a[i] and right_min > a[i]):
+            answer += 1
+    return answer
