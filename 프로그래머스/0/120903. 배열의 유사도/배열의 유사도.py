73114903587d11e555bd6e79919dd7a53e87e875
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-06-04 22:02:44 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -0,0 +1,10 @@
+def solution(s1, s2):
+    answer = 0
+    
+    for a in s1:
+        for b in s2:
+            if a == b:
+                answer += 1
+                break
+    
+    return answer
