c731c9f8ed1abe3ad09b87685a0811ea3801693d
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-06-06 22:22:14 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,12 @@
+def solution(my_string, is_prefix):
+
+    prefix = []
+    for i in range(1,len(my_string)):
+        prefix.append(my_string[:i])
+    
+    if is_prefix in prefix:
+        answer = 1
+    else:
+        answer = 0
+        
+    return answer
