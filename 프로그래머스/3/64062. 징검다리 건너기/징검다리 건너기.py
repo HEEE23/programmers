93bf119777852abbfc0d7644d5b77d0dc849e578
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-06-08 22:34:59 +0900

    [level 3] Title: 징검다리 건너기, Time: 380.52 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,46 +1,71 @@
 def solution(stones, k):
     answer = 0
     
-    # 이진 탐색 
-    start = 1
-    end = max(stones)
+    left = 1
+    right = max(stones)
     
-    while start <= end:
-        cnt = 0
-        # 건너는 인원
-        mid = (start + end) // 2
-
-        for stone in stones:
-            if (stone - mid) <= 0:
-                cnt += 1
-                if cnt >= k:
-                    break
+    while left <= right:
+        mid = (left+right) // 2
+        print(mid)
+        ni = 0
+        for s in stones:
+            if s - mid < 0:
+                ni +=1 
             else:
-                cnt = 0
-            
-        if cnt < k:
-            start = mid + 1
+                ni = 0
+                
+            if ni >= k:
+                break
+        
+        if ni >= k:
+            right = mid -1
         else:
             answer = mid
-            end = mid - 1
+            left = mid + 1
+    return answer
+# def solution(stones, k):
+#     answer = 0
     
-#     # 정확성만 통과, 효율성 x
-#     while True:
+#     # 이진 탐색 
+#     start = 1
+#     end = max(stones)
+    
+#     while start <= end:
 #         cnt = 0
-#         answer += 1   
-        
-#         for i in range(len(stones)):
-#             if stones[i] != 0:
-#                 stones[i] -= 1
-#             else:
-#                 continue
-        
+#         # 건너는 인원
+#         mid = (start + end) // 2
+
 #         for stone in stones:
-#             if stone == 0:
+#             if (stone - mid) <= 0:
 #                 cnt += 1
-#                 if cnt == k:
-#                     return answer
+#                 if cnt >= k:
+#                     break
 #             else:
 #                 cnt = 0
+            
+#         if cnt < k:
+#             start = mid + 1
+#         else:
+#             answer = mid
+#             end = mid - 1
+    
+# #     # 정확성만 통과, 효율성 x
+# #     while True:
+# #         cnt = 0
+# #         answer += 1   
+        
+# #         for i in range(len(stones)):
+# #             if stones[i] != 0:
+# #                 stones[i] -= 1
+# #             else:
+# #                 continue
+        
+# #         for stone in stones:
+# #             if stone == 0:
+# #                 cnt += 1
+# #                 if cnt == k:
+# #                     return answer
+# #             else:
+# #                 cnt = 0
 
-    return answer
+#     return answer
