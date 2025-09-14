8b7c0e133264d84c6777f775ada1317f77fc6e5f
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2025-09-14 12:08:01 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,15 @@
 def solution(array, commands):
     answer = []
-    result = []
-    for i in commands:
-        result = array[i[0]-1:i[1]]
-        result.sort()
-        answer.append(result[i[2]-1])
+    
+    for c in commands:
+        result = sorted(array[c[0]-1:c[1]])
+        answer.append(result[(c[2]-1)%len(result)])
+        
+        
+    # result = []
+    # for i in commands:
+    #     result = array[i[0]-1:i[1]]
+    #     result.sort()
+    #     answer.append(result[i[2]-1])
         
     return answer
