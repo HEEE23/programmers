8af870f89435279ae2ea66f995eb7057260df3fc
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2026-09-19 10:46:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.89 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 11.48 ms
+메모리: 11.5 MB, 시간: 12.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 22:56:07
+2026년 09월 19일 10:46:09
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,62 @@
+# from collections import deque
+# def solution(maps):
+#     n = len(maps)
+#     m = len(maps[0])
+    
+#     visited = [[0]*m for _ in range(n)]
+    
+#     queue = deque()
+#     queue.append((0,0))
+#     visited[0][0] = 1
+    
+#     dx = [-1,1,0,0]
+#     dy = [0,0,1,1]
+#     while queue:
+#         x, y = queue.popleft()
+        
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+            
+#             if 0 <= nx < n and 0 <= ny < m:
+#                 if visited[nx][ny] == 0 and maps[nx][ny] == 1:
+#                     visited[nx][ny] = 1
+#                     maps[nx][ny] = maps[x][y] + 1
+#                     queue.append((nx,ny))
+                    
+#     if maps[n-1][m-1] == 1:
+#         return -1
+#     else:
+#         return maps[n-1][m-1]
+
 from collections import deque
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    visited = [[False]*m for i in range(n)]
     
-    # 위, 아래, 왼, 오
+    visited = [[0]*m for i in range(n)]
+    
+    queue = deque()
+    queue.append((0,0))
+    visited[0][0] = 1
+    
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-    
-    q = deque()
-    q.append((0,0))
-    
-    visited[0][0] = True
-    while q:
-        x, y = q.popleft()
+    while queue:
+        x, y = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                if not visited[nx][ny]:
-                    visited[nx][ny] == True
-                    q.append((nx,ny))
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny] == 1 and not visited[nx][ny]:
                     maps[nx][ny] = maps[x][y] + 1
-                    
+                    queue.append((nx,ny))
+                    visited[nx][ny] = 1
+
     if maps[n-1][m-1] == 1:
         return -1
     else:
         return maps[n-1][m-1]
+    return
